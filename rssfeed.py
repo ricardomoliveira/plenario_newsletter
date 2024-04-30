d09674c5b79ff622af27efa01211a0de40e1bf05
Ricardo M. Oliveira <ricardomeoliveira@gmail.com>
2024-04-30 11:06:06 +0100

    Update rssfeed.py
    Catch new items

--- rssfeed.py
+++ rssfeed.py
@@ -1,14 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import feedparser
 import ssl
 from bs4 import BeautifulSoup
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
+# Define the UTC offset for 8 am
+UTC_OFFSET = timedelta(hours=8)
+
 # RSS feeds to fetch
 DR_FEED = ['http://files.dre.pt/rss/serie1.xml',
-                         'https://files.dre.pt/rss/serie2&parte=a.xml',
-                         'https://files.dre.pt/rss/serie2&parte=c.xml']
+           'https://files.dre.pt/rss/serie2&parte=a.xml',
+           'https://files.dre.pt/rss/serie2&parte=c.xml']
 
 SNS_FEED = 'https://www.sns.gov.pt/noticias/feed/'
 
@@ -20,9 +23,14 @@ INFARMED_FEED = ['https://www.infarmed.pt/web/infarmed/rss-agenda/-/asset_publis
 GOV_FEED = ['https://rssproxy.migor.org/api/w2f?v=0.1&url=https%3A%2F%2Fwww.portugal.gov.pt%2Fpt%2Fgc24%2Fcomunicacao%2Fintervencoes&link=.%2Fdiv%5B1%5D%2Fa%5B1%5D&context=%2F%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B4%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv&re=none&q=contains(%23any%2C%20%22Sa%C3%BAde%22)&out=atom',
             'https://rssproxy.migor.org/api/w2f?v=0.1&url=https%3A%2F%2Fwww.portugal.gov.pt%2Fpt%2Fgc24%2Fgoverno%2Fcomunicados-do-conselho-de-ministros&link=.%2Fdiv%5B1%5D%2Fa%5B1%5D&context=%2F%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B4%5D%2Fdiv%5B1%5D%2Fdiv%5B2%5D%2Fdiv&re=none&q=contains(%23any%2C%20%22sa%C3%BAde%22)&out=atom',
             'https://rssproxy.migor.org/api/w2f?v=0.1&url=https%3A%2F%2Fwww.portugal.gov.pt%2Fpt%2Fgc24%2Fcomunicacao%2Fnoticias&link=.%2Fdiv%5B1%5D%2Fa%5B1%5D&context=%2F%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv%5B4%5D%2Fdiv%5B1%5D%2Fdiv%5B1%5D%2Fdiv&re=none&q=contains(%23any%2C%20%22Sa%C3%BAde%22)&out=atom']
+
 def fetch_new_items(rss_feed_urls):
     rss_items = []
     current_date = datetime.now().date()
+    previous_date = current_date - timedelta(days=1)
+
+    # Calculate the datetime for 8 am UTC of the previous day
+    previous_day_8am_utc = datetime.combine(previous_date, datetime.min.time()) + UTC_OFFSET
 
     # Iterate over each RSS feed URL
     for rss_feed_url in rss_feed_urls:
@@ -36,14 +44,13 @@ def fetch_new_items(rss_feed_urls):
                     # Parse the publication date of the item
                     pub_date = datetime.strptime(item.published, "%Y-%m-%dT%H:%M:%S%z").date()
 
-                    # Check if the item is from today
-                    if pub_date == current_date:
+                    # Check if the item is from the previous day after 8 am UTC
+                    if pub_date == previous_date and datetime.strptime(item.published, "%Y-%m-%dT%H:%M:%S%z") > previous_day_8am_utc:
                         # Extract relevant information from the item
                         title = item.title
-                        #description = item.description
                         link = item.link
                         # Add the item to the list of new items
-                        rss_items.append({"title": title, "description": description, "link": link, "pubDate": pub_date})
+                        rss_items.append({"title": title, "link": link, "pubDate": pub_date})
                 elif "rssproxy" in rss_feed_url:
                     # Parse the publication date of the item
                     html_content = item.content[0].value
@@ -52,8 +59,8 @@ def fetch_new_items(rss_feed_urls):
                     if date_tag:
                         date_str = date_tag.text.strip()
                         pub_date = datetime.strptime(date_str, '%Y-%m-%d às %Hh%M').date()
-                        # Check if the item is from today
-                        if pub_date == current_date:
+                        # Check if the item is from the previous day after 8 am UTC
+                        if pub_date == previous_date and datetime.strptime(date_str, '%Y-%m-%d às %Hh%M') > previous_day_8am_utc:
                             # Extract relevant information from the item
                             title = item.title
                             link = item.link
@@ -62,11 +69,12 @@ def fetch_new_items(rss_feed_urls):
                 else:
                     # Extract relevant information from the item
                     title = item.title
-                    description = item.description
+                    #description = item.description
                     link = item.link
                     # Add the item to the list of new items
-                    if "Saúde" in title or "saúde" in title or "Saúde" in description or "saúde" in description:
-                        rss_items.append({"title": title, "description": description, "link": link})
+                    if "Saúde" in title or "saúde" in title:
+                        rss_items.append({"title": title, "link": link})
+
         except Exception as e:
             print(f"An error occurred while fetching or parsing the feed '{rss_feed_url}': {e}")
 
@@ -78,3 +86,6 @@ def main():
     dr_items = fetch_new_items(DR_FEED)
     infarmed_items = fetch_new_items(INFARMED_FEED)
     return dr_items, gov_items, sns_items, infarmed_items
+
+if __name__ == "__main__":
+    main()
